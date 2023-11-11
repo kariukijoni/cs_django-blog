e16c1c49eb8c6a56d37b6b56627e45641a06a9d9
kariukijoni <kariukijoni@gmail.com>
2023-11-11 19:02:25 +0300

    updated settings

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hhtc^h4py1iz(cdol_)ztggu0v+u&w3_^xxdescse4qno#-nv8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cschaferblog.herokuapp.com']
 
 
 # Application definition
